e6ced819cbd9479306e5512058654c4af9a3d901
Granit Bunjaku <granitb@live.com>
2026-05-02 13:40:11 +0200

    Fix iOS push (APNs registration) + build 18

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,24 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    FirebaseApp.configure()
+
+    UNUserNotificationCenter.current().delegate = self
+
+    application.registerForRemoteNotifications()
+
+    GeneratedPluginRegistrant.register(with: self)
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
