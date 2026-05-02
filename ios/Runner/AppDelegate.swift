b24db795e64326a0689e00bc5b4542d63571aabc
Granit Bunjaku <granitb@live.com>
2026-05-02 15:24:21 +0200

    Fix iOS AppDelegate crash

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,23 @@
-import UIKit
 import Flutter
-import Firebase
+import UIKit
+import FirebaseCore
 import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
     FirebaseApp.configure()
 
     UNUserNotificationCenter.current().delegate = self
-
     application.registerForRemoteNotifications()
 
-    GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
