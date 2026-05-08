8c4a4f4fcaad9dc67898744b66866739368b529b
Granit Bunjaku <granitb@live.com>
2026-05-08 22:03:35 +0200

    Revert iOS AppDelegate crash fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,37 +1,16 @@
 import Flutter
 import UIKit
-import Firebase
-import FirebaseMessaging
-import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    FirebaseApp.configure()
-
-    UNUserNotificationCenter.current().delegate = self
-
-    application.registerForRemoteNotifications()
-
-    GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    Messaging.messaging().apnsToken = deviceToken
-
-    super.application(
-      application,
-      didRegisterForRemoteNotificationsWithDeviceToken: deviceToken
-    )
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
